788ea08fa19b24963732814462ed680cc1b88724
insvrg3ncy <gunsshow9@gmail.com>
2026-07-23 17:45:14 +0300

    tests

--- Content.IntegrationTests/Tests/GameObjects/Components/Mobs/AlertsComponentTests.cs
+++ Content.IntegrationTests/Tests/GameObjects/Components/Mobs/AlertsComponentTests.cs
@@ -91,6 +91,8 @@ namespace Content.IntegrationTests.Tests.GameObjects.Components.Mobs
                 var alertControls = clientAlertsUI.AlertContainer.Children.Select(c => (AlertControl) c);
                 var alertIDs = alertControls.Select(ac => ac.Alert.ID).ToArray();
                 // SD edit start
+                // IPC uses BorgHealth instead of HumanHealth.
+                var expectedHealthIDs = new[] { "HumanHealth", "BorgHealth" };
                 var expectedDebugIDs = new[] { "Debug1", "Debug2" };
 
                 Assert.That(alertIDs, Is.SupersetOf(expectedDebugIDs));
@@ -112,6 +114,8 @@ namespace Content.IntegrationTests.Tests.GameObjects.Components.Mobs
                 var alertControls = clientAlertsUI.AlertContainer.Children.Select(c => (AlertControl) c);
                 var alertIDs = alertControls.Select(ac => ac.Alert.ID).ToArray();
                 // SD edit start
+                // IPC uses BorgHealth instead of HumanHealth.
+                var expectedHealthIDs = new[] { "HumanHealth", "BorgHealth" };
                 var expectedDebugIDs = new[] { "Debug2" };
 
                 Assert.That(alertIDs, Is.SupersetOf(expectedDebugIDs));
